b5ce79ccdd0c6fea9095b3819e0a70956d3fdeb6
Thierno Abasse DIALLO <diallo980211@gmail.com>
2025-12-21 16:41:29 +0100

    Saisie et exception

--- td1/exo-intro.py
+++ td1/exo-intro.py
@@ -1,3 +1,5 @@
+import math
+
 # Definition des variable
 texte = "Hello World"
 print(f"Le type de texte est : {type(texte)}")
@@ -13,3 +15,26 @@ texte , entier , dec = "Affectation dans la meme ligne", 11, 3.14
 print(f"Type de texte : {type(texte)}")
 print(f"Type de entier : {type(entier)}")
 print(f"Type de decimal : {type(dec)}")
+
+# Saisie des valeurs
+rayon = float(input("Entrez le rayon du cone : "))
+hauteur = float(input("Entrez la hauteur du cone : "))
+
+volume = 1/3 * math.pi * math.pow(rayon, 2) * hauteur
+
+print(f"Le volume du cone est : {volume:.2f}")
+
+a=3
+print(type(a))
+
+print(type(a == 3))
+
+chaine = "Le chat"
+nombre = 3
+print(f"{chaine} + {nombre}")
+
+try:
+    valeur = int(input("Entrez un nombre : "))
+    print(f"Valeur saisie : {valeur}")
+except ValueError:
+    print("La valeur saisie n'est pas valide")
