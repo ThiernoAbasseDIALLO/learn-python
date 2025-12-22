20a587820369f7021e36d0ab7e00b5dc9f580629
Thierno Abasse DIALLO <diallo980211@gmail.com>
2025-12-22 15:04:09 +0100

    Boucle et conditions

---
+++ td1/boucle.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    print(f"J'écoute le prof...(iteration {i+1})")
+
+annee = 10
+while annee >= 1:
+    if annee == 1:
+        print("C’est dans 1 an je m’en irai j’entends le loup le renard chanter")
+    else:
+        print(f"C’est dans {annee} ans je m’en irai j’entends le loup le renard chanter")
+    annee -= 1
